5c77ddc03f6231855771595b5452d430778a13cf
tsutaya07 <97420531+tsutaya07@users.noreply.github.com>
2022-01-13 12:19:49 +0700

    Ex4

---
+++ Assignments/Exercise4_Nattawut_S.py
@@ -0,0 +1,10 @@
+ScoreForEng  = 10
+ScoreGeBus   = 50
+ScoreInComs  = 90
+ScoreComPro  = 60
+
+print("---Your Score---")
+print("Foundation English:", ScoreForEng)
+print("General Business:", ScoreGeBus)
+print("Introduction to Computer System:", ScoreInComs)
+print("Computer Programming:", ScoreComPro) 
