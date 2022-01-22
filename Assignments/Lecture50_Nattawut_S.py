751d3f4fa7ea4d28a57df5ce8836c3580294b6ad
tsutaya07 <97420531+tsutaya07@users.noreply.github.com>
2022-01-22 12:26:39 +0700

    Lecture50

---
+++ Assignments/Lecture50_Nattawut_S.py
@@ -0,0 +1,17 @@
+
+x = int(input("Please Enter number:"))
+y = int(input("Please Enter number:"))
+
+def plusNumber(x,y):
+    print("x+y =",x+y)
+def minusNumber(x,y):
+    print("x-y =",x-y)
+def multiplyNumber(x,y):
+    print("x * y =",x*y)
+def divideNumber(x,y):
+    print("x / y =",x/y)
+
+plusNumber(x,y)
+minusNumber(x,y)
+multiplyNumber(x,y)
+divideNumber(x,y)
